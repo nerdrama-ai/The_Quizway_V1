99afc30756dd44225129e90e94fc2491eeb4ca68
nerdrama.ai <nerdrama.ai@gmail.com>
2025-08-31 21:51:08 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Header({onGoAdmin,onGoHome}){
 return (
 <header className="header">
-<h1 className="logo" onClick={onGoHome}>📦 QuizBox</h1>
+<h1 className="logo" onClick={onGoHome}>📦 The Quizway</h1>
 <div>
 <button className="btn" onClick={onGoAdmin}>Admin</button>
 </div>
